4a954e389a8670952ce75e457903aeb6661c1974
Mauricio García Soto <mauriciojaviergarcia@iesbetxi.es>
2025-11-17 17:12:12 +0100

    classe

--- app/src/main/java/com/example/newplayjuegos/MainActivity.kt
+++ app/src/main/java/com/example/newplayjuegos/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.newplayjuegos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val jugador = findViewById(R.id.button2) as Button
+        jugador.setOnClickListener{lanzarNewPlayer()}
+    }
+    private fun lanzarNewPlayer() {
+        val i = Intent(this, NewPlayer::class.java)
+        startActivity(i)
     }
 
 }
---
+++ app/src/main/java/com/example/newplayjuegos/NewPlayer.kt
@@ -0,0 +1,5 @@
+package com.example.newplayjuegos
+
+class NewPlayer {
+
+}
