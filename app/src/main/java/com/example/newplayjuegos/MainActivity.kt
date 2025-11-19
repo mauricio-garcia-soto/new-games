650ac21cc633d8b667cb1ac65f583a87c5cb67d1
Mauricio García Soto <mauriciojaviergarcia@iesbetxi.es>
2025-11-19 17:39:17 +0100

    classe

--- app/src/main/java/com/example/newplayjuegos/MainActivity.kt
+++ app/src/main/java/com/example/newplayjuegos/MainActivity.kt
@@ -5,18 +5,36 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.content.Intent
+import android.view.Menu
+import android.content.ClipData.Item
+import android.view.MenuItem
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val jugador = findViewById(R.id.button2) as Button
         jugador.setOnClickListener{lanzarNewPlayer()}
     }
     private fun lanzarNewPlayer() {
         val i = Intent(this, NewPlayer::class.java)
         startActivity(i)
+    }
+    public override fun onCreateOptionsMenu(menu:Menu): Boolean{
+        getMenuInflater().inflate(R.menu.menu_main,menu)
+        return true
+    }
+    public override fun onOptionsItemSelected(item:MenuItem): Boolean{
+     var id = item.getItemId()
+        if (id==R.id.action_buscar){
+            Toast.makeText(this,"busqueda", Toast.LENGTH_LONG).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
--- app/src/main/java/com/example/newplayjuegos/NewPlayer.kt
+++ app/src/main/java/com/example/newplayjuegos/NewPlayer.kt
@@ -1,5 +1,30 @@
 package com.example.newplayjuegos
 
-class NewPlayer {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class NewPlayer: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newplayer)
+        val toolbar= findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    public override fun onOptionsItemSelected(item:MenuItem): Boolean{
+        val id = item.getItemId()
+        if (id==R.id.action_buscar){
+            Toast.makeText(this,"busqueda", Toast.LENGTH_LONG).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
